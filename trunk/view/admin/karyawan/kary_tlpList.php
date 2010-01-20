bcdd22bd864f07b60bf41aa3267ab9b54bf485fc
freezy.off <freezy.off@07aa1f8e-f377-11de-acd2-8fd6687fc178>
2010-01-20 08:19:18 +0000

    gk dipake

--- trunk/view/admin/karyawan/kary_tlpList.php
+++
@@ -1,5 +0,0 @@
-<tr id="<?=$trID?>" bgcolor="#FFF">
-	<td><?=$tlp?><input name="tlpArr[]" type="hidden" value="<?=$tlp?>" /></td>
-	<td><a onclick="tlp_delete('<?=$trID?>', '<?=$tlp?>')">Delete</a></td>
-	<td width="10"></td>
-</tr>
--- trunk/view/admin/laporan_periodeGraph/periodeGraph_periodeCombo.php
+++
@@ -1,8 +0,0 @@
-<?php $periodeLoop = false ?>
-<?php $PERIODE = periode_select(); ?>
-<?php while ($row = mysql_fetch_assoc($PERIODE) ):?>
-<?php		if ($periodeLoop):?>
-	<option value="<?=$row['ID_PERIODE']?>" <?=$periodeEnd===$row['ID_PERIODE']? "selected=\"selected\"" : ""?>><?=$row['ID_PERIODE']?></option>
-<?php		endif; ?>
-<?php		if ($periodeStart===$row['ID_PERIODE']) $periodeLoop=true; ?>
-<?php endwhile; ?>
